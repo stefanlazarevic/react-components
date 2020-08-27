118aa9459f3b4bca3076284673353f5dd6a67459
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2020-08-27 18:38:58 +0200

    [#031]
    Moved storybook to dark theme.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,18 @@
 import '../src/index.scss';
 
+import { themes } from '@storybook/theming';
+import theme from './theme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: { expanded: true }
+  controls: { expanded: true },
+  docs: {
+    theme: themes.dark,
+  },
+  backgrounds: {
+    values: [
+      { name: 'light', value: '#f6f7fb' },
+      { name: 'dark', value: '#1f2229' },
+    ],
+  },
 }
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,38 +1,7 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  base: 'light',
+  base: 'dark',
 
-  colorPrimary: 'white',
-  colorSecondary: 'silver',
-
-  // UI
-  appBg: 'white',
-  appContentBg: '#1F2229',
-  appBorderColor: 'grey',
-  appBorderRadius: 4,
-  appContentTextColor: 'white',
-
-  // Typography
-  fontBase: '"Open Sans", sans-serif',
-  fontCode: 'monospace',
-
-  // Text colors
-  textColor: 'black',
-  textInverseColor: 'rgba(255,255,255,0.9)',
-
-  // Toolbar default and active colors
-  barTextColor: 'silver',
-  barSelectedColor: 'black',
-  barBg: 'white',
-
-  // Form colors
-  inputBg: 'white',
-  inputBorder: 'silver',
-  inputTextColor: 'black',
-  inputBorderRadius: 4,
-
-  brandTitle: 'My custom storybook',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://placehold.it/350x150',
-});
+  appContentBg: '#191c28'
+})
